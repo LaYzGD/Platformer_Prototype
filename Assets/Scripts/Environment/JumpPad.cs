45436a568903fbf6d47d9a5a32d1717e8cdf4761
LaYzGD <sollynobiuz@gmail.com>
2024-07-25 17:32:58 +0200

    JumpPad

---
+++ Assets/Scripts/Environment/JumpPad.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class JumpPad : ITriggerable
+{
+    [SerializeField] private GameObject _jumpPadGraphics;
+    [SerializeField] private BoxCollider2D _collider2D;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private bool _isOn;
+
+    private void Start()
+    {
+        _jumpPadGraphics.SetActive(_isOn);
+        _collider2D.enabled = _isOn;
+    }
+
+    public override void Trigger()
+    {
+        _isOn = !_isOn;
+        _jumpPadGraphics.SetActive(_isOn);
+        _collider2D.enabled = _isOn;
+    }
+
+    public override void UnTrigger()
+    {
+        _isOn = !_isOn;
+        _jumpPadGraphics.SetActive(_isOn);
+        _collider2D.enabled = _isOn;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!_isOn)
+        {
+            return;
+        }
+
+        if (collision.gameObject.TryGetComponent(out Rigidbody2D rigidbody2D))
+        {
+            if (collision.gameObject.TryGetComponent(out Player player))
+            {
+                player.InAirState.SetIsJumping();
+            }
+            rigidbody2D.AddForce(transform.up * _jumpForce * rigidbody2D.mass, ForceMode2D.Impulse);
+        }
+    }
+}
