e8280fa329732419f7bea2e658232f7bcfe01843
LaYzGD <sollynobiuz@gmail.com>
2024-07-25 17:17:50 +0200

    FanButtonActivation

--- Assets/Scripts/Environment/Fan.cs
+++ Assets/Scripts/Environment/Fan.cs
@@ -1,11 +1,24 @@
 using UnityEngine;
 
-public class Fan : MonoBehaviour
+public class Fan : ITriggerable
 {
     [SerializeField] private float _force;
+    [SerializeField] private GameObject _fanEffects;
     [SerializeField] private ForceType _forceType;
+    [SerializeField] private bool _isOn;
+
+    private void Start()
+    {
+        _fanEffects.SetActive(_isOn);
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
+        if (!_isOn)
+        {
+            return;
+        }
+
         if (collision.TryGetComponent(out IForceControllable forceControllable))
         {
             forceControllable.ControlForce(transform.up * _force, _forceType);
@@ -14,9 +27,26 @@ public class Fan : MonoBehaviour
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        if (!_isOn)
+        {
+            return;
+        }
+
         if (collision.TryGetComponent(out IForceControllable forceControllable))
         {
             forceControllable.StopForceControl();
         }
+    }
+
+    public override void Trigger()
+    {
+        _isOn = !_isOn;
+        _fanEffects.SetActive(_isOn);
+    }
+
+    public override void UnTrigger()
+    {
+        _isOn = !_isOn;
+        _fanEffects.SetActive(_isOn);
     }
 }
