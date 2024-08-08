1c78eed9a6175ba46aec2a0d4769f45832d0c7fb
LaYzGD <sollynobiuz@gmail.com>
2024-08-08 15:40:21 +0200

    AudioFix

--- Assets/Scripts/Audio/PlayerAudioEffects.cs
+++ Assets/Scripts/Audio/PlayerAudioEffects.cs
@@ -4,6 +4,15 @@ public class PlayerAudioEffects : MonoBehaviour
 {
     [SerializeField] private AudioSource _audioSource;
     [SerializeField] private AudioSource _loopAudioSource;
+    [SerializeField][Range(0.5f, 1.2f)] private float _minPitch;
+    [SerializeField][Range(0.5f, 1.2f)] private float _maxPitch;
+
+    private float _defaultPitch;
+
+    private void Start()
+    {
+        _defaultPitch = _audioSource.pitch;
+    }
 
     public void PlayLoopSound(AudioClip clip)
     {
@@ -18,11 +27,13 @@ public class PlayerAudioEffects : MonoBehaviour
 
     public void PlaySound(AudioClip clip)
     {
+        _audioSource.pitch = Random.Range(_minPitch, _maxPitch);
         _audioSource.PlayOneShot(clip);
     }
 
     public void PlayRandomClip(AudioClip[] clips) 
     {
+        _audioSource.pitch = Random.Range(_minPitch, _maxPitch);
         _audioSource.PlayOneShot(clips[Random.Range(0, clips.Length)]);
     }
 }
